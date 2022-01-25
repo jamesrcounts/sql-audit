19dc4ab829fe742b80e28ff7b0294ebfdaa061f0
Jim Counts <jamesrcounts@outlook.com>
2022-01-25 20:52:53 +0000

    Add a test database

--- infrastructure/azure/sql.tf
+++ infrastructure/azure/sql.tf
@@ -27,12 +27,31 @@ resource "azurerm_mssql_database_extended_auditing_policy" "master" {
   log_monitoring_enabled = true
 }
 
+
+resource "azurerm_mssql_database" "test" {
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  name           = "acctest-db-d"
+  read_scale     = true
+  server_id      = azurerm_mssql_server.example.id
+  sku_name       = "BC_Gen5_2"
+  tags           = local.tags
+  zone_redundant = true
+}
+
+resource "azurerm_mssql_database_extended_auditing_policy" "test" {
+  database_id            = azurerm_mssql_database.test.id
+  log_monitoring_enabled = true
+}
+
 module "diagnostics" {
   source                     = "github.com/jamesrcounts/terraform-modules.git//diagnostics?ref=main"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
 
   monitored_services = {
     "audit-db" = local.database_id
+    "test-db"  = azurerm_mssql_database.test.id
     sql        = azurerm_mssql_server.example.id
   }
 }
